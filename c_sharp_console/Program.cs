cfecb049ab557274afb7ff3572a226af5b106f03
r-serori <ryunosuke.sakaue@gmail.com>
2024-08-29 09:43:04 +0900

    initial com

---
+++ c_sharp_console/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace c_sharp_console
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      string[,] table = new string[3, 3];
+      bool isBreak = false;
+
+      for (int i = 0; i < table.GetLength(0); i++)
+      {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+          if (isBreak)
+          {
+            table[i, j] = "X";
+            isBreak = false;
+            Console.WriteLine(table[i, j]);
+          }
+          else
+          {
+            table[i, j] = "O";
+            isBreak = true;
+            Console.WriteLine(table[i, j]);
+          }
+
+        }
+      }
+
+    }
+  }
+}
